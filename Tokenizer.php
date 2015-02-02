c023ed810b5c1d189112775c3f337855a4f82111
Daan Wilmer BSc <daan@daanwilmer.nl>
2015-02-02 09:46:10 +0100

    Created Tokenizer class

---
+++ Tokenizer.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Tokenizer class, to turn a string into a list of tokens
+ */
+
+class Tokenizer {
+	public static $SPACE = ' ';
+	public static $TAB = "\t";
+	public static $NEWLINE = "\r?\n";
+	public static $WHITESPACE = '[\s]+';
+	private $excludedTokens;
+	private $includedTokens;
+
+	public function __construct() {
+		$this->excludedTokens = array();
+		$this->includedTokens = array();
+	}
+
+	public function addBoundary($boundary, $isToken = false, $preventEscape = false) {
+		// escape boundary
+		if(!$preventEscape) {
+			$boundary = str_replace(
+				array('.','[',']','\\'),
+				array('\.','\[', '\]','\\\\'), $boundary);
+		}
+		if($isToken) {
+			$this->includedTokens[] = $boundary;
+		} else {
+			$this->excludedTokens[] = $boundary;
+		}
+	}
+
+	public function addBoundaries($boundaries, $areTokens = false) {
+		foreach($boundaries as $boundary) {
+			$this->addBoundary($boundary, $areTokens);
+		}
+	}
+
+
+	public function tokenize($string) {
+		// split on excluded tokens
+		if(count($this->excludedTokens) > 0) {
+			$pattern = '/' . implode('|', $this->excludedTokens) . '/';
+			$parts = preg_split($pattern, $string, null, PREG_SPLIT_NO_EMPTY);
+		} else {
+			$parts = array($string);
+		}
+
+		// split on included tokens
+		if(count($this->excludedTokens) > 0) {
+			$tokens = array();
+			$pattern = '/(' . implode(')|(', $this->includedTokens) . ')/';
+			foreach($parts as $part) {
+				$newtokens = preg_split($pattern, $part, null, PREG_SPLIT_DELIM_CAPTURE | PREG_SPLIT_NO_EMPTY);
+				$tokens = array_merge($tokens, $newtokens);
+			}
+		} else {
+			$tokens = $parts;
+		}
+		return $tokens;
+	}
+
+}
+
+
+$testString = "Dit is een test. Dit is, jazeker,een test.";
+$tokenizer = new Tokenizer();
+$tokenizer->addBoundary(' ');
+$tokenizer->addBoundaries(array('.', ','), true);
+print_r($tokenizer->tokenize($testString));
+
